9b75c52b900ced6ddd5a0697674400fc0aeca28f
Kevin Baxter <kbax6342@gmail.com>
2016-10-18 15:43:33 -0400

    Working About Page

--- library-script.js
+++ library-script.js
@@ -34,5 +34,17 @@ var books =[
   author:'Paula Hawkins',
   description:'A psychological thriller set in the environs of London is full of complications and betrayals.',
   canCheckOutBook: true
+},
+{
+  name:'The Girls',
+  author: 'Emma Cline',
+  description: 'Northern California, during the violent end of the 1960s. At the start of summer, a lonely and thoughtful teenager, Evie Boyd, sees a group of girls in the park, and is immediately caught by their freedom, their careless dress, their dangerous aura of abandon. Soon, Evie is in thrall to Suzanne, a mesmerizing older girl, and is drawn into the circle of a soon-to-be infamous cult and the man who is its charismatic leader. Hidden in the hills, their sprawling ranch is eerie and run down, but to Evie, it is exotic, thrilling, charged—a place where she feels desperate to be accepted. As she spends more time away from her mother and the rhythms of her daily life, and as her obsession with Suzanne intensifies, Evie does not realize she is coming closer and closer to unthinkable violence.',
+  canCheckOutBook: true
+},
+{
+  name: 'Modern Lovers',
+  author: 'Emma Straub',
+  description: 'Friends and former college bandmates Elizabeth and Andrew and Zoe have watched one another marry, buy real estate, and start businesses and families, all while trying to hold on to the identities of their youth. But nothing ages them like having to suddenly pass the torch (of sexuality, independence, and the ineffable alchemy of cool) to their own offspring.',
+  canCheckOutBook: true
 }
 ];
